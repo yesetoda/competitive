19ecb4fed02dcfbbd4159364310dad6f8d91258c
yesetoda <107122034+yesetoda@users.noreply.github.com>
2024-08-13 15:17:52 +0300

    Time: 235 ms (20.51%), Space: 23.3 MB (70.84%) - LeetHub

--- 1428-jump-game-iii/1428-jump-game-iii.py
+++ 1428-jump-game-iii/1428-jump-game-iii.py
@@ -11,9 +11,13 @@ class Solution:
                 if arr[cur] == 0:
                     return True
                 if cur+arr[cur]<leng and not cur+arr[cur] in seen:
+                    if arr[cur+arr[cur]] == 0:
+                        return True
                     q.append(cur+arr[cur])
                     seen.add(cur+arr[cur])
                 if cur-arr[cur]>=0 and not cur-arr[cur] in seen:
+                    if arr[cur-arr[cur]] == 0:
+                        return True
                     q.append(cur-arr[cur])
                     seen.add(cur-arr[cur])
             
