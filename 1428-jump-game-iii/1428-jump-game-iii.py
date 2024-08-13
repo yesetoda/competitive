ee875f41ee8c4f9af4a9f0c1cbf8c94169b4025d
yesetoda <107122034+yesetoda@users.noreply.github.com>
2024-08-13 15:17:02 +0300

    Time: 226 ms (49.25%), Space: 23.6 MB (44.5%) - LeetHub

---
+++ 1428-jump-game-iii/1428-jump-game-iii.py
@@ -0,0 +1,22 @@
+class Solution:
+    def canReach(self, arr: List[int], start: int) -> bool:
+        leng = len(arr)
+        q = deque([start])
+        seen = set()
+        while q:
+            ln = len(q)
+            for _ in range(ln):
+                cur = q.popleft()
+                seen.add(cur)
+                if arr[cur] == 0:
+                    return True
+                if cur+arr[cur]<leng and not cur+arr[cur] in seen:
+                    q.append(cur+arr[cur])
+                    seen.add(cur+arr[cur])
+                if cur-arr[cur]>=0 and not cur-arr[cur] in seen:
+                    q.append(cur-arr[cur])
+                    seen.add(cur-arr[cur])
+            
+        return False
+
+                
