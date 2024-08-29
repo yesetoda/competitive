56bf88207100d9fc1c4b0e582bfdc0ada8c68052
yesetoda <107122034+yesetoda@users.noreply.github.com>
2024-08-29 10:47:32 +0300

    Time: 117 ms (91.4%), Space: 17.3 MB (71.81%) - LeetHub

--- 0984-most-stones-removed-with-same-row-or-column/0984-most-stones-removed-with-same-row-or-column.py
+++ 0984-most-stones-removed-with-same-row-or-column/0984-most-stones-removed-with-same-row-or-column.py
@@ -1,17 +1,38 @@
 class Solution:
     def removeStones(self, stones: List[List[int]]) -> int:
-        seenRow,seenCol = set(),set()
-        cnt = 0
-        for r,c in stones:
-            if r in seenRow:
-                cnt+=1
-                seenCol.add(c)
-            elif c in seenCol:
-                cnt+=1
-                seenRow.add(r)
+        parent =  [i for i in range(len(stones))]
+        size = [1]*len(stones)
+        ans = 0
+        def find(x):
+            if x != parent[x]:
+                x = find(parent[x])
+            return parent[x] 
+        def union(x,y):
+            nonlocal ans
+            xrep = find(x)
+            yrep = find(y)
+            if xrep == yrep:
+                return
+            if size[xrep]>=size[yrep]:
+                parent[yrep]= xrep
+                size[xrep]+=size[yrep]
             else:
-                seenRow.add(r)
-                seenCol.add(c)
-        return cnt
-            
+                parent[xrep]= yrep
+                size[yrep]+=size[xrep]
+            ans +=1
+        x,y = {},{}
+        for ind,(i,j) in enumerate(stones):
+            if i in x:
+                union(ind,x[i])
+            else:
+                x[i] = ind
+            if j in y:
+                union(ind,y[j])
+            else:
+                y[j] = ind
+        
+        return ans
+
+
+
         
