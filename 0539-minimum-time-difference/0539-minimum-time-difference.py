2f6e8b124dd83dc275eabb34e92190eb69bfff81
yesetoda <107122034+yesetoda@users.noreply.github.com>
2024-09-16 09:50:55 +0300

    Time: 80 ms (18.09%), Space: 19.6 MB (36.73%) - LeetHub

--- 0539-minimum-time-difference/0539-minimum-time-difference.py
+++ 0539-minimum-time-difference/0539-minimum-time-difference.py
@@ -6,17 +6,12 @@ class Solution:
         for i in range(leng):
             prev= timePoints[i-1].split(":")
             now = timePoints[i].split(":")
-            next = timePoints[(i+1)%leng].split(":")
             prev = int(prev[0])*60+int(prev[1])
             now = int(now[0])*60+int(now[1])
-            next = int(next[0])*60+int(next[1])
             dif1 = abs(prev-now)
-            dif2 = abs(now-next)
             if dif1>=12*60:
                 dif1 = 24*60 - prev + now
-            if dif2>=12*60:
-                dif2 = 24*60 - next + now
-            mn = min(mn,dif1,dif2)
+            mn = min(mn,dif1)
         return mn
             
 
