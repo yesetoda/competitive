0036984c4c93012ca22877355673af2a67c17e9e
yesetoda <107122034+yesetoda@users.noreply.github.com>
2024-10-01 13:01:48 +0300

    Time: 585 ms (5.07%), Space: 30.5 MB (57.83%) - LeetHub

--- 1620-check-if-array-pairs-are-divisible-by-k/1620-check-if-array-pairs-are-divisible-by-k.py
+++ 1620-check-if-array-pairs-are-divisible-by-k/1620-check-if-array-pairs-are-divisible-by-k.py
@@ -1,24 +1,20 @@
 class Solution:
     def canArrange(self, arr: List[int], k: int) -> bool:
-
-        x = []
-        for i in arr:
+        for i in range(len(arr)):
             if i<0:
-                x.append((k-((-i)%k))%k)
-            else:
-                x.append(i%k)
-        x.sort()
+                arr[i] = k - abs(arr[i])%k
+            arr[i]= arr[i]%k
+        arr.sort()
         ind = 0
-        while ind <len(x):
-            left = bisect_left(x,x[ind])
-            right = bisect_right(x,x[ind])
-            if x[ind] == 0:
+        while ind <len(arr):
+            left = bisect_left(arr,arr[ind])
+            right = bisect_right(arr,arr[ind])
+            if arr[ind] == 0:
                 if (right - left)%2:
                     return False
             else:
-                mate_left = bisect_left(x,k-x[ind])
-                mate_right = bisect_right(x,k-x[ind])
-
+                mate_left = bisect_left(arr,k-arr[ind])
+                mate_right = bisect_right(arr,k-arr[ind])
                 if right-left != mate_right-mate_left:
                     return False
             ind = right
