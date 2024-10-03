bf84cb89165003769538ec0fc56f15b0b6b2ed2e
yesetoda <107122034+yesetoda@users.noreply.github.com>
2024-10-03 22:18:01 +0300

    Time: 35 ms (80.58%), Space: 16.7 MB (15.91%) - LeetHub

--- 0755-reach-a-number/0755-reach-a-number.py
+++ 0755-reach-a-number/0755-reach-a-number.py
@@ -1,12 +1,11 @@
 class Solution:
     def reachNumber(self, target: int) -> int:
         target = abs(target)
-        a = 1
-        b = 1
         c = -2*target
-        ans1  = -b + (b**2 - 4*a*c)**(0.5)/(2*a)
-        ans2  = -b - (b**2 - 4*a*c)**(0.5)/(2*a)
-        ans = int(max(ans1,ans2))
+        part = (1 - 4*c)**0.5
+        root1  = (-1 + part)/2
+        root2  = (-1 - part)/2
+        ans = int(max(root1,root2))
         val = ans*(ans+1)//2
         while val<target or (target-val)%2:
             ans+=1
