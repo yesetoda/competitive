1f3397d058802b087bc9cebb03f52db15735601f
yesetoda <107122034+yesetoda@users.noreply.github.com>
2024-10-24 17:46:53 +0300

    Time: 0 ms (100%), Space: 16.6 MB (39.2%) - LeetHub

--- 0028-find-the-index-of-the-first-occurrence-in-a-string/0028-find-the-index-of-the-first-occurrence-in-a-string.py
+++ 0028-find-the-index-of-the-first-occurrence-in-a-string/0028-find-the-index-of-the-first-occurrence-in-a-string.py
@@ -1,28 +1,26 @@
 class Solution:
     def strStr(self, haystack: str, needle: str) -> int:
-        pt1 = 0
-        pt2 = 0
-        candidate = -1
-        cnt = 0
-        while pt1<len(haystack):
-            if pt2>0:
-                if haystack[pt1] == needle[0]:
-                    if candidate<0:
-                        candidate = pt1
-            if pt2==len(needle):
-                return pt1-pt2
-            if haystack[pt1] == needle[pt2]:
-                pt1+=1
-                pt2+=1
-            else:
-                if candidate>0:
-                    pt1 = candidate
-                    candidate = -1
-                else:
-                    pt1+=1
-                pt2 = 0
-            cnt+=1
-
-        if pt2==len(needle):
-            return pt1-pt2
+        n,k = len(haystack),len(needle)
+        a , b = 3 , 10**9+7
+        alphas = [pow(a,k-i-1,b) for i in range(k)]
+        def char_val(x):
+            return ord(x)-96
+        def pollFirst(hash,sv):
+            return ( hash- alphas[ 0 ] * sv ) % b
+        def addLast(hash,sv):
+            return (hash*a +  sv ) % b
+        vals = mine = 0
+        if k>n:
+            return -1
+        for i in range(k):
+            vals = addLast(vals,char_val(needle[i]))
+            mine =  addLast(mine,char_val(haystack[i]))
+        for i in range(k,n):
+            if mine == vals:
+                return i-k
+            mine =  pollFirst(mine,char_val(haystack[i-k]))
+            mine =  addLast(mine,char_val(haystack[i]))
+        if mine == vals:
+            return n-k
         return -1
+            
