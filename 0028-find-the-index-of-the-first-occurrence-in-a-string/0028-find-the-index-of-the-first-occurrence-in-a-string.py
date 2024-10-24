f0b3b055454519622199e660b164b9926ce31633
yesetoda <107122034+yesetoda@users.noreply.github.com>
2024-10-24 21:16:52 +0300

    Time: 0 ms (100%), Space: 16.6 MB (39.2%) - LeetHub

--- 0028-find-the-index-of-the-first-occurrence-in-a-string/0028-find-the-index-of-the-first-occurrence-in-a-string.py
+++ 0028-find-the-index-of-the-first-occurrence-in-a-string/0028-find-the-index-of-the-first-occurrence-in-a-string.py
@@ -1,26 +1,32 @@
 class Solution:
     def strStr(self, haystack: str, needle: str) -> int:
-        n,k = len(haystack),len(needle)
-        a , b = 3 , 10**9+7
-        alphas = [pow(a,k-i-1,b) for i in range(k)]
-        def char_val(x):
-            return ord(x)-96
-        def pollFirst(hash,sv):
-            return ( hash- alphas[ 0 ] * sv ) % b
-        def addLast(hash,sv):
-            return (hash*a +  sv ) % b
-        vals = mine = 0
-        if k>n:
-            return -1
-        for i in range(k):
-            vals = addLast(vals,char_val(needle[i]))
-            mine =  addLast(mine,char_val(haystack[i]))
-        for i in range(k,n):
-            if mine == vals:
-                return i-k
-            mine =  pollFirst(mine,char_val(haystack[i-k]))
-            mine =  addLast(mine,char_val(haystack[i]))
-        if mine == vals:
-            return n-k
-        return -1
-            
+        a,b = 0,1
+        lsp =[0]
+        nleng,hleng = len(needle),len(haystack)
+        while b<nleng:
+            if needle[a] == needle[b]:
+                lsp.append(a+1)
+                a+=1
+                b+=1
+            elif a>0:
+                a = lsp[a-1]
+            else:
+                b+=1
+                lsp.append(0)
+        i = j = 0
+        ans = -1
+        while i<hleng:
+            if j == nleng:
+                ans = i-j
+                break
+            if haystack[i] == needle[j]:
+                i+=1
+                j+=1
+            elif j>0:
+                j = lsp[j-1]
+            else:
+                i+=1
+        if j == nleng:
+            ans = i-j
+
+        return ans
