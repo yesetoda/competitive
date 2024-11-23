56cd4cdd265db87b98b0ae0cecd425617ad5de3b
Yeneineh Seiba <107122034+yesetoda@users.noreply.github.com>
2024-11-23 09:12:17 +0300

    Time: 91 ms (5.87%), Space: 22.9 MB (10.46%) - LeetHub

--- 0134-gas-station/0134-gas-station.py
+++ 0134-gas-station/0134-gas-station.py
@@ -1,23 +1,19 @@
 class Solution:
     def canCompleteCircuit(self, gas: List[int], cost: List[int]) -> int:
-        go = []
         leng= len(gas)
         path = deque()
-        for i in range(leng):
-            go.append(gas[i]-cost[i])
         l = r = 0
         sm = 0
         while  l>=0:
             if sm>=0:
-                path.append(go[r])
-                sm+=go[r]
-                r+=1
-                r%=leng
+                val = gas[r]-cost[r]
+                path.append(val)
+                sm+=val
+                r = (r + 1) % leng
             else:
                 while sm<0:
                     sm -= path.popleft()
                     l+=1
-
                     if l == leng:
                         l = -1
                         break
